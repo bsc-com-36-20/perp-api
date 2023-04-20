932d1bcb98b92dbbb0538a253b5afbcfb524f298
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2023-04-20 10:43:38 +0200

    endpoint Patch has been created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 //const connection = mysql.createCo
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 // Request Body parsing Middleware
 app.use(bodyParser.json());  
 
@@ -42,6 +42,8 @@ app.post('/Students', (req,res) => {
                   }
                 });
               });
+             
+
 // getting student by id
 app.get('/Students/:id', (req,res)  => {
     const id = req.params.id;
@@ -57,8 +59,42 @@ app.get('/Students/:id', (req,res)  => {
       }
       });
     });
-  
+    
 
+// Route handler for PATCH /students/:id
+
+app.patch('/api/vi/posts/:id', function (request, response)  {
+  const sql =`SELECT * FROM Students WHERE id = ${request.params.id} LIMIT 1`
+  return dbConnection.query(sql, function(err,rows){
+    if (err) throw err
+    const post= request.body 
+   // console.log(post)
+if (rows.length >= 1){
+  let props = []
+  props = Object.keys(post).map((key, index) => {
+    return `${key}='${post[key]}'`
+  })
+  const updateSql = `UPDATE Students SET ${props.join(',')} WHERE id=${rows[0].id}`
+  return dbConnection.query(updateSql, function(err,result){
+    if (err) throw err
+    if (result.affectedRows === 0) {
+      return response.status(404).json({
+        status:false,
+        statusCode:404,
+        message:  `Post with id ${request.params.id} does not exist`
+      })
+    }
+    return response.status(200).json({
+      status:true,
+      statusCode:200,
+      message:  'Post updated successfully'
+    })
+  })
+}
+  
+})
+})
+ 
 
 app.listen(3000,function(){
     console.log('perp listening on port 3000')
